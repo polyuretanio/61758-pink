40aec7f219629c833cd8d9487717ba0f6f4c9718
Nakleikoff <a@htmlacademy.ru>
2015-09-17 15:04:17 +0300

    :cat: Мяу-мяу. Обновления от Кекса для модуля «Препроцессоры, часть 1»

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,15 @@ module.exports = function(grunt) {
   require('load-grunt-tasks')(grunt);
 
   var config = {
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
+
+    sass: {
+      style: {
+        files: {
+          'css/style.css': 'sass/style.scss'
+        }
+      }
+    }
   };
 
   config = require('./.gosha')(grunt, config);
